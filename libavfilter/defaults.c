jiaxianhua/ffmpeg-svn

commit 0089497c6974201da8d21596a980fa11a063b83a

Remove useless free, fix warning: libavfilter/defaults.c: In function 'avfilter_default_get_audio_buffer': libavfilter/defaults.c:129: warning: 'buf' is used uninitialized in this function
git-svn-id: svn://svn.ffmpeg.org/ffmpeg/trunk@26382 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavfilter/defaults.c
@@ -126,7 +126,6 @@ AVFilterBufferRef *avfilter_default_get_audio_buffer(AVFilterLink *link, int per
     return ref;
 
 fail:
-    av_free(buf);
     if (ref && ref->audio)
         av_free(ref->audio);
     av_free(ref);
@@ -210,6 +209,7 @@ int avfilter_default_config_output_link(AVFilterLink *link)
         if (link->type == AVMEDIA_TYPE_VIDEO) {
             link->w = link->src->inputs[0]->w;
             link->h = link->src->inputs[0]->h;
+            link->sample_aspect_ratio = link->src->inputs[0]->sample_aspect_ratio;
             link->time_base = link->src->inputs[0]->time_base;
         } else if (link->type == AVMEDIA_TYPE_AUDIO) {
             link->channel_layout = link->src->inputs[0]->channel_layout;

commit 20df6ef68d65c38141639294b073a2f120032626

Fix fate breakage in r26382.
git-svn-id: svn://svn.ffmpeg.org/ffmpeg/trunk@26383 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavfilter/defaults.c
@@ -209,7 +209,6 @@ int avfilter_default_config_output_link(AVFilterLink *link)
         if (link->type == AVMEDIA_TYPE_VIDEO) {
             link->w = link->src->inputs[0]->w;
             link->h = link->src->inputs[0]->h;
-            link->sample_aspect_ratio = link->src->inputs[0]->sample_aspect_ratio;
             link->time_base = link->src->inputs[0]->time_base;
         } else if (link->type == AVMEDIA_TYPE_AUDIO) {
             link->channel_layout = link->src->inputs[0]->channel_layout;